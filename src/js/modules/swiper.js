63cf53e75d4860f82d611e7a1283df4188797c4e
Татьяна <ltasya72@gmail.com>
2024-11-13 11:45:55 +0200

    docs

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,12 +1,14 @@
 // import Swiper JS
 import Swiper from "swiper/bundle";
 // import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 // import Swiper styles
 // import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/bundle";
-
+// import { Autoplay } from "swiper/modules";
+// Swiper.use(Autoplay);
+let swipper1;
 function swiperFunk() {
   const swiper = new Swiper(".section-projects__slider", {
     modules: [Navigation],
@@ -41,6 +43,42 @@ function swiperFunk() {
     //   },
     // },
   });
-}
 
+  const configswiper1 = {
+    modules: [Pagination],
+    pagination: {
+      el: ".gallery-swiper__pagination",
+      clickable: true,
+      dinamicBullets: true,
+    },
+
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+  };
+
+  const breakpoint = window.matchMedia("(max-width:768px)");
+  const breakpointChecker = function () {
+    if (breakpoint.matches === true) {
+      // console.log(breakpoint.matches);
+      const swipper1 = new Swiper("#gallery-swiper", configswiper1);
+      return;
+    } else if (breakpoint.matches === false) {
+      // console.log(breakpoint.matches);
+      if (swipper1 !== undefined) swipper1.destroy(true, true);
+      return;
+    }
+  };
+
+  breakpoint.addListener(breakpointChecker);
+  breakpointChecker();
+}
 export default swiperFunk;
